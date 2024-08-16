afac008ed1aa5da4f6ae14f26dc6c415a994422e
Sorita Heng <69398037+soritaheng@users.noreply.github.com>
2024-08-16 22:10:13 +1000

    move files to frontend folder

---
+++ frontend/src/views/ChooseRepo.jsx
@@ -0,0 +1,10 @@
+const ChooseRepo = () => {
+  return (
+    <>
+      <h1 className="font-bold text-xl">Choose your repository</h1>
+      <div></div>
+    </>
+  );
+};
+
+export default ChooseRepo;
