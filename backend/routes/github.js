786e5fa210689cb83d2726f9501ee24b425d58f9
Hyunsoo6257 <h28.park@connect.qut.edu.au>
2024-08-17 01:56:49 +1000

    add cors

--- backend/routes/github.js
+++ backend/routes/github.js
@@ -1,13 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
-router.get('/repos/:username', async function (req, res, next) {
+router.get("/repos/:username", async function (req, res, next) {
   try {
-    const username = req.params['username']; // Capture the username from the URL parameter
+    const username = req.params["username"]; // Capture the username from the URL parameter
 
     if (!username) {
-      return res.status(400).json({ error: 'Username is required' });
+      return res.status(400).json({ error: "Username is required" });
     }
 
     const github_url = `https://api.github.com/users/${username}/repos?per_page=100&type=all`;
@@ -16,24 +16,22 @@ router.get('/repos/:username', async function (req, res, next) {
     const data = await response.json();
 
     if (!response.ok) {
-      console.error('GitHub API Error:', data);
+      console.error("GitHub API Error:", data);
       return res.status(response.status).json({ error: data.message });
     }
 
     // Map over the response data to extract only the required fields
-    const filteredData = data.map(repo => ({
+    const filteredData = data.map((repo) => ({
       id: repo.id,
       name: repo.name,
-      url: repo.html_url
+      url: repo.html_url,
     }));
 
     res.status(200).json(filteredData);
   } catch (error) {
-    console.error('Error:', error.message);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error:", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
 module.exports = router;
-
-
