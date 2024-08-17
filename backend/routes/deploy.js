cae8e7b5aa6d7f68ed7e35b3a82ddced51ccae15
Neel Dave <neeldave2002@gmail.com>
2024-08-17 17:21:24 +1000

    Deploys the Site

--- backend/routes/deploy.js
+++ backend/routes/deploy.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const crypto = require('crypto');
+const crypto = require("crypto");
 const fs = require("fs");
 require("dotenv").config();
 const router = express.Router();
@@ -28,97 +28,104 @@ router.get("/", async (req, res) => {
       });
 
     async function getFilesInFolder(folderPath) {
-        const files = [];
-      
-        // Recursively list all files
-        fs.readdirSync(folderPath).forEach((file) => {
-          const filePath = path.join(folderPath, file);
-          if (fs.statSync(filePath).isFile()) {
-            files.push(filePath);
-          } else {
-            files.push(...getFilesInFolder(filePath));
-          }
-        });
-      
-        return files;
-      }
-      
-      const files = await getFilesInFolder(filesFolder);
-      
-      // Assuming there will always be 3 files
-      const indexHtmlPath = files.find(file => file.endsWith('.html'));
-      const stylesCssPath = files.find(file => file.endsWith('.css'));
-      const scriptJsPath = files.find(file => file.endsWith('.js'));
-      
-      // Read the file contents
-      const indexHtmlContent = await fs.promises.readFile(indexHtmlPath, 'utf8');
-      const stylesCssContent = await fs.promises.readFile(stylesCssPath, 'utf8');
-      const scriptJsContent = await fs.promises.readFile(scriptJsPath, 'utf8');
-      
-      const index_base64Content = Buffer.from(indexHtmlContent).toString('base64');
-      const style_base64Content = Buffer.from(stylesCssContent).toString('base64');
-      const script_base64Content = Buffer.from(scriptJsContent).toString('base64');
-      
+      const files = [];
+
+      // Recursively list all files
+      fs.readdirSync(folderPath).forEach((file) => {
+        const filePath = path.join(folderPath, file);
+        if (fs.statSync(filePath).isFile()) {
+          files.push(filePath);
+        } else {
+          files.push(...getFilesInFolder(filePath));
+        }
+      });
+
+      return files;
+    }
+
+    const files = await getFilesInFolder(filesFolder);
+
+    // Assuming there will always be 3 files
+    const indexHtmlPath = files.find((file) => file.endsWith(".html"));
+    const stylesCssPath = files.find((file) => file.endsWith(".css"));
+    const scriptJsPath = files.find((file) => file.endsWith(".js"));
+
+    // Read the file contents
+    const indexHtmlContent = await fs.promises.readFile(indexHtmlPath, "utf8");
+    const stylesCssContent = await fs.promises.readFile(stylesCssPath, "utf8");
+    const scriptJsContent = await fs.promises.readFile(scriptJsPath, "utf8");
+
+    const index_base64Content =
+      Buffer.from(indexHtmlContent).toString("base64");
+    const style_base64Content =
+      Buffer.from(stylesCssContent).toString("base64");
+    const script_base64Content =
+      Buffer.from(scriptJsContent).toString("base64");
+
     await octokit.request(`PUT /repos/${owner}/${repo}/contents/index.html`, {
+      owner: owner,
+      repo: repo,
+      path: `index.html`,
+      message: "my commit message",
+      committer: {
+        name: "Neel Dave",
+        email: "neeldave0910@gmail.com",
+      },
+      content: index_base64Content,
+      headers: {
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
+    });
+
+    await octokit.request(`PUT /repos/${owner}/${repo}/contents/style.css`, {
+      owner: owner,
+      repo: repo,
+      path: `style.css`,
+      message: "my commit message",
+      committer: {
+        name: "Neel Dave",
+        email: "neeldave0910@gmail.com",
+      },
+      content: style_base64Content,
+      headers: {
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
+    });
+
+    await octokit.request(`PUT /repos/${owner}/${repo}/contents/script.js`, {
+      owner: owner,
+      repo: repo,
+      path: `script.js`,
+      message: "my commit message",
+      committer: {
+        name: "Neel Dave",
+        email: "neeldave0910@gmail.com",
+      },
+      content: script_base64Content,
+      headers: {
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
+    });
+
+    const github_page_data = await octokit.request(
+      `POST /repos/${owner}/${repo}/pages`,
+      {
         owner: owner,
         repo: repo,
-        path: `index.html`,
-        message: 'my commit message',
-        committer: {
-          name: 'Neel Dave',
-          email: 'neeldave0910@gmail.com'
+        source: {
+          branch: "main"
         },
-        content: index_base64Content,
         headers: {
-          'X-GitHub-Api-Version': '2022-11-28'
-        }
-      })
-
-      await octokit.request(`PUT /repos/${owner}/${repo}/contents/style.css`, {
-        owner: owner,
-        repo: repo,
-        path: `style.css`,
-        message: 'my commit message',
-        committer: {
-          name: 'Neel Dave',
-          email: 'neeldave0910@gmail.com'
+          "X-GitHub-Api-Version": "2022-11-28",
         },
-        content: style_base64Content,
-        headers: {
-          'X-GitHub-Api-Version': '2022-11-28'
-        }
-      })
-
-      await octokit.request(`PUT /repos/${owner}/${repo}/contents/script.js`, {
-        owner: owner,
-        repo: repo,
-        path: `script.js`,
-        message: 'my commit message',
-        committer: {
-          name: 'Neel Dave',
-          email: 'neeldave0910@gmail.com'
-        },
-        content: script_base64Content,
-        headers: {
-          'X-GitHub-Api-Version': '2022-11-28'
-        }
-      })
-
-    
-
-   
-
-    await res.send(
-      `files uploaded!`
+      }
     );
+
+    await res.send(github_page_data);
   } catch (error) {
     console.error("Error creating repository:", error);
     res.status(500).send("Error creating repository");
   }
 });
-
-
-  
-
 
 module.exports = router;
