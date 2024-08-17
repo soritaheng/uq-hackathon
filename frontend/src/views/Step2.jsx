a79bdc493478fc2bd35cf155b7cf02dc8bd7dc37
Sorita Heng <69398037+soritaheng@users.noreply.github.com>
2024-08-17 14:01:58 +1000

    update styling step2 page

--- frontend/src/views/Step2.jsx
+++ frontend/src/views/Step2.jsx
@@ -3,6 +3,7 @@ import { Checkbox, CheckboxGroup } from "@chakra-ui/react";
 import { Stack } from "@chakra-ui/react";
 import { RepoContext } from "../components/RepoContext";
 import { useNavigate } from "react-router-dom";
+import ButtonPrimary from "../components/ButtonPrimary";
 
 function Step2() {
   const { repos, setRepos } = useContext(RepoContext);
@@ -12,43 +13,52 @@ function Step2() {
   // Initialize checked items from repos context
   useEffect(() => {
     if (repos && repos.length > 0) {
-      setCheckedItems(repos.filter(repo => repo.included).map(repo => repo.id.toString()));
+      setCheckedItems(repos.map((repo) => repo.id.toString()));
+    } else {
+      return (
+        <p>No repositories found. Please go back and fetch repositories.</p>
+      );
     }
   }, [repos]);
 
   const handleNextStep = () => {
-    const selectedRepos = repos.map(repo => ({
+    const selectedRepos = repos.map((repo) => ({
       ...repo,
-      included: checkedItems.includes(repo.id.toString())
+      included: checkedItems.includes(repo.id.toString()),
     }));
     setRepos(selectedRepos);
     console.log(selectedRepos); // Logs the updated repos with the 'included' property
-    navigate('/step3');
+    navigate("/step3");
   };
 
   const checkItems = (values) => {
     setCheckedItems(values);
   };
 
-  if (repos.length === 0) return <p>No repositories found. Please go back and fetch repositories.</p>;
-
   return (
-    <div>
-      <h1 className="font-bold text-xl">Step 2: GitHub Repositories</h1>
-      <CheckboxGroup
-        colorScheme="green"
-        value={checkedItems}
-        onChange={checkItems}
-      >
-        <Stack spacing={1} direction={["column"]}>
-          {repos.map((repo) => (
-            <Checkbox key={repo.id} value={repo.id.toString()}>
-              {repo.name}
-            </Checkbox>
-          ))}
-        </Stack>
-      </CheckboxGroup>
-      <button onClick={handleNextStep}>Next</button>
+    <div className="text-center space-y-10">
+      <h1 className="font-bold text-heading text-primary">
+        Choose your repositories
+      </h1>
+      <div className="max-w-[600px] mx-auto border p-8 border-primary rounded-3xl overflow-y-auto max-h-[600px]">
+        <CheckboxGroup
+          colorScheme="primary"
+          value={checkedItems}
+          onChange={checkItems}
+        >
+          <Stack spacing={1} direction={["column"]}>
+            {repos.map((repo) => (
+              <Checkbox key={repo.id} value={repo.id.toString()}>
+                {repo.name}
+              </Checkbox>
+            ))}
+          </Stack>
+        </CheckboxGroup>
+      </div>
+      <ButtonPrimary
+        eventHandler={handleNextStep}
+        label={"Next"}
+      ></ButtonPrimary>
     </div>
   );
 }
